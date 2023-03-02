a4595ef67304ecacafbaebbefdcf641ee1797262
antiduhring <83927931+antiduhring@users.noreply.github.com>
2023-03-02 02:59:38 +0100

    first commit

---
+++ src/test/kotlin/antiduhring/gallery/GalleryApplicationTests.kt
@@ -0,0 +1,13 @@
+package antiduhring.gallery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
